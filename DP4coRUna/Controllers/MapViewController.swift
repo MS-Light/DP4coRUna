8a48a3e2d1d29b1d56708a7898841a58211f50f9
bestkobyna <36083088+bestkobyna@users.noreply.github.com>
2020-08-06 18:46:05 -0400

    location permission

--- DP4coRUna/Controllers/MapViewController.swift
+++ DP4coRUna/Controllers/MapViewController.swift
@@ -6,16 +6,43 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager: CLLocationManager?
+    
+    @IBOutlet weak var Map: MKMapView!
     override func viewWillAppear(_ animated: Bool) {
         navigationController?.isNavigationBarHidden = true
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+
+        view.backgroundColor = .gray
         // Do any additional setup after loading the view.
+        var currentLoc: CLLocation!
+        if(CLLocationManager.authorizationStatus() == .authorizedWhenInUse ||
+        CLLocationManager.authorizationStatus() == .authorizedAlways) {
+           currentLoc = locationManager?.location
+           print(currentLoc.coordinate.latitude)
+           print(currentLoc.coordinate.longitude)
+        }
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                if CLLocationManager.isRangingAvailable() {
+                    // do staff
+                    print("location permission granted")
+                }
+            }
+        }
+    }
 }
 
