5d39b3c8f6b7c3ea2446e9ed07c6f6071773705e
sdasmaha <srinjoy.dasmahapatra@rutgers.edu>
2020-08-22 20:37:23 -0400

    Notification Controller

---
+++ DP4coRUna/Controllers/NotificationController.swift
@@ -0,0 +1,47 @@
+//
+//  NotificationController.swift
+//  DP4coRUna
+//
+//  Created by Srinjoy DasMahapatra on 8/22/20.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+
+class NotificationController: UIViewController{
+    
+    override func viewDidLoad() {
+        
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options:[.alert, .sound])
+            { (granted, error) in
+            
+            }
+        
+        // Step2 : Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "DP4coRUna - You might have been exposed"
+        content.body = "Go take a Covid Test Soon"
+        
+        // Step3: Create the notification trigger
+        let date = Date().addingTimeInterval(5)
+        
+        let dateComponents = Calendar.current.dateComponents([.year,.month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //Step 4:  Create the request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content , trigger: trigger)
+        
+        
+        //Step 5:  Register the request
+        center.add(request) { (error) in
+            }
+    }
+}
